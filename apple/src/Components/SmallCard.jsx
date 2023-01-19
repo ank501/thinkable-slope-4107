348d43ceeaebf357ec061518b725b751b1376e63
ank501 <ankitsyadav246@gmail.com>
2023-01-19 22:59:00 +0530

    day 3

---
+++ apple/src/Components/SmallCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Card, CardHeader, CardBody, CardFooter ,Text ,Heading ,Stack,Image} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
+
+const SmallCard = ({image,title,subtitle,avail,learn,bgCol,textColor}) => {
+  return (
+    <Card background={bgCol}  >
+        <CardHeader>
+           <Heading size={"3xl"} textColor={textColor}>{title}</Heading>
+           
+           <Heading marginTop={"20px"} size="lg" textColor={textColor}>{subtitle}</Heading>
+           
+        </CardHeader>
+  <CardBody>
+  
+    <Image
+      src= {image}
+      alt='Green double couch with wooden legs'
+      borderRadius='lg'
+    />
+  </CardBody>
+</Card>
+  )
+}
+
+export default SmallCard
